367a1c6ad40e1b58ef0170fbeb3caab3f939adf7
YoruLi <lionel3327@gmail.com>
2023-12-23 17:20:22 -0300

    fix issues

--- components/tiles.tsx
+++ components/tiles.tsx
@@ -14,13 +14,21 @@ export default function Tiles({
 
     eventSource.onmessage = (event) => {
       const data = JSON.parse(event.data);
-      if (data.type === "delete") {
-        grid.value = data.tiles;
-      } else {
-        const { index, color } = data;
-        const gridValue = grid.value;
-        grid.value = gridValue.with(index, color);
-      }
+      const { index, color } = data;
+      const gridValue = grid.value;
+      grid.value = gridValue.with(index, color);
+    };
+
+    return () => eventSource.close();
+  }, []);
+
+  useEffect(() => {
+    const eventSource = new EventSource("/api/listen-delete");
+
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      grid.value = data.tiles;
     };
 
     return () => eventSource.close();
--- shared/db.ts
+++ shared/db.ts
@@ -38,7 +38,7 @@ export const getGrid = async (): Promise<Grid> => {
 
 export const clearGrid = async () => {
   const pixels = db.list<string>({ prefix: [KEYS_DB.tiles] });
-  const bc = new BroadcastChannel("PIXEL_UPDATE");
+  const bc = new BroadcastChannel("PIXEL_DELETE");
 
   for await (const pixel of pixels) {
     await db.delete([KEYS_DB.tiles, pixel.key[1]]);
